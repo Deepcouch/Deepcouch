a159dd21b2ec1e0765709c8dc19cc015635ea3da
“fabiorocco” <fabio.rocco@green-conseil.com>
2016-12-15 12:51:08 +0100

    Add structure of the header.php

---
+++ wp-content/themes/deepcouch/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<img src="logo.png">
+			<ul>
+				<li>
+					<a href="">films</a>
+				</li>
+				<li>
+					<a href="">series</a>
+				</li>
+				<li>
+					<a href="">actualites</a>
+				</li>
+				<li>
+					<a href="">acteurs</a>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<a href="">profil</a>
+				</li>
+				<li>
+					<a href="">recherche</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
