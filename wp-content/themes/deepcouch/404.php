50bf0586c284ba698d230f4c2b9a0a71aa86d390
fabiorocco <fabio.rocco@green-conseil.com>
2017-02-24 01:36:48 +0100

    Add 404 page

--- wp-content/themes/deepcouch/404.php
+++ wp-content/themes/deepcouch/404.php
@@ -1,1 +1,6 @@
-WALA GROS
+<?php get_header(); ?>
+<div class="error-404">
+    <h1>NO WAY</h1>
+    <a class="btn black-text white" href="<?php echo bloginfo('url'); ?>">Retour au site</a>
+</div>
+<?php get_footer(); ?>
